dd97bf9b94da1eb9ab1094d1b9384d5c23fc2eb7
Yin-Vannthy <thygod942@gmail.com>
2024-08-31 20:47:50 +0700

    first commit

---
+++ src/main/java/com/api/keycloak/configuration/BeanConfig.java
@@ -0,0 +1,14 @@
+package com.api.keycloak.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
---
+++ src/main/java/com/api/keycloak/model/request/PasswordRequest.java
@@ -0,0 +1,12 @@
+package com.api.keycloak.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class PasswordRequest {
+    private String password;
+}
